506b47119e721ad9dd974ca4fd7dc80a5d116e6b
MERT METİN <mertmetin1993@gmail.com>
2024-03-17 20:21:15 +0300

    feature: Adding airline-getbykey endpoint method and download nuget package for Couchbase

---
+++ Controllers/AirlinesController.cs
@@ -0,0 +1,33 @@
+﻿using Couchbase;
+using Couchbase.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace CouchifyTravelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirlinesController(INamedBucketProvider buckedProvider) : ControllerBase
+    {
+        private readonly INamedBucketProvider _buckedProvider = buckedProvider;
+
+        [HttpGet]
+        public async Task<string> Get(string key)
+        {
+            try
+            {
+                var bucket = await _buckedProvider.GetBucketAsync();
+                var scope = bucket.Scope("inventory");
+                var collection = scope.Collection("airline");
+
+                JObject result = (await collection.GetAsync(key)).ContentAs<JObject>();
+                return $"Fetch document success. Result: {result}";
+            }
+            catch (CouchbaseException ex)
+            {
+                return ex.Message;
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Couchbase.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCouchbase(builder.Configuration.GetSection("Couchbase"));
+builder.Services.AddCouchbaseBucket<INamedBucketProvider>("travel-sample");
 
 var app = builder.Build();
 
